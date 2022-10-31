c357323a737b56076baaeccf5a147a1e910d1f86
BenjaminVilladsen <benmicvil@gmail.com>
2022-10-31 10:29:57 +0100

    api req workin

--- policyhub/src/components/page-components/MainSection.jsx
+++ policyhub/src/components/page-components/MainSection.jsx
@@ -1,9 +1,36 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
 import './MainSection.css';
 import CustomerCard from '../CustomerCard';
+import Axios from 'axios';
 
 const MainSection = () => {
+
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    getAllCustomers();
+    console.log(customers)
+  }, []);
+  
+
+  //fetching all customers from the database
+  const getAllCustomers = async () =>{
+    Axios.get("http://130.225.39.66:8080/customers", )
+      .then((res) => {
+        setCustomers(res.data);
+        console.log(res.data);
+      }
+    )
+  }
+
+  
+
+
+
+
+
   return (
     <>
       <Row className='mainsection mx-5 my-5'>
@@ -19,12 +46,15 @@ const MainSection = () => {
             </select>
           </div>
           <div className="displaycustomers">
+
             <CustomerCard
               name='Benjamin Franklin'
               type="Person"
               address='Benjavej 24'
               city="Copenhagen"
             />
+          </div>
+          <div className='test'>
           </div>
         </Col>
       </Row>
