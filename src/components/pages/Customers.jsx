c53618e0656eb6e32c7ed7fe48418bc8ee7bf134
Emil Gram <emilgram2001@gmail.com>
2022-11-02 15:58:04 +0100

    updated customers

--- src/components/pages/Customers.jsx
+++ src/components/pages/Customers.jsx
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { useEffect, useState, } from "react";
-import {Link, Navigate, useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 import personLogo from "../../assets/person.png";
 import firmLogo from "../../assets/firm.png";
@@ -40,7 +40,6 @@ const Customers = () => {
       address: "1234 Main Street",
       type: "person",
     },
-   
   ];
 
   useEffect(() => {
@@ -56,7 +55,6 @@ const Customers = () => {
     });
   };
 
-
   const navigate = useNavigate();
 
   const handleRowClick = (row) => {
@@ -67,31 +65,77 @@ const Customers = () => {
   return (
     <div className="customers">
       <div className="customer-cards">
-        
-        {/* {dummyCustomers.map((customer) => (
-          <Link to={customer.id}>
-          <CustomerCard key={customer.id} customer={customer} />
-          </Link>
-        ))} */}
+        <div className="customer-card-search row">
+          <div className="col-4">
+            <div class="mb-3">
+              <label for="disabledSelect" class="form-label">
+                Search
+              </label>
+              <input
+                type="text"
+                placeholder="Search"
+                className="form-control customer-card-searchbar"
+              />
+            </div>
+          </div>
+          <div className="col-4">
+            <div class="mb-3">
+              <label for="disabledSelect" class="form-label">
+                Filter by
+              </label>
+              <select id="disabledSelect" class="form-select">
+                <option>Filter by</option>
+              </select>
+            </div>
+          </div>
+          <div className="col-4">
+            <div class="mb-3">
+              <label for="disabledSelect" class="form-label">
+                Sort by
+              </label>
+              <select id="disabledSelect" class="form-select">
+                <option>Sort by</option>
+              </select>
+            </div>
+          </div>
+          <hr />
+        </div>
         <table className="customer-table table table-bordered">
           <thead>
             <tr>
-              <th className="p-4 customer-table-image" scope="col">Type</th>
-              <th className="p-4" scope="col">Name</th>
-              <th className="p-4" scope="col">Customer ID</th>
-              <th className="p-4" scope="col">Address</th>
+              <th className="p-4 customer-table-image" scope="col">
+                Type
+              </th>
+              <th className="p-4" scope="col">
+                Name
+              </th>
+              <th className="p-4" scope="col">
+                Customer ID
+              </th>
+              <th className="p-4" scope="col">
+                Address
+              </th>
             </tr>
           </thead>
           <tbody>
             {dummyCustomers.map((customer) => (
-              
-
-              <tr key={customer.id} className=" my-4" onClick={()=> handleRowClick(customer)}>
-                
-                <td className="p-4"><img height={40} src={customer.type == "person" ? personLogo : firmLogo } alt="" /></td>
-                <td className="p-4" >{customer.name} <br /> {customer.surName}</td>
+              <tr
+                key={customer.id}
+                className=" my-4"
+                onClick={() => handleRowClick(customer)}
+              >
+                <td className="p-4">
+                  <img
+                    height={40}
+                    src={customer.type == "person" ? personLogo : firmLogo}
+                    alt=""
+                  />
+                </td>
+                <td className="p-4">
+                  {customer.name} <br /> {customer.surName}
+                </td>
                 <td className="p-4">{customer.id}</td>
-                <td className="p-4" >{customer.address}</td>
+                <td className="p-4">{customer.address}</td>
               </tr>
             ))}
           </tbody>
