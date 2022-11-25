40d55393a540985ea055c1336c3540484ec19b83
BenjaminVilladsen <benmicvil@gmail.com>
2022-11-25 10:47:04 +0100

    animation

--- src/components/pages/Loading.jsx
+++ src/components/pages/Loading.jsx
@@ -2,6 +2,11 @@ import phub from '../../assets/phub.png';
 import AnimatedPage from '../AnimatedPage';
 import sound from '../../assets/intro.mp3';
 
+//import framer motion
+import { motion } from "framer-motion";
+
+//import {ReactComponent as AnimatedLogo} from '../../assets/logo_animated.svg';
+
 import "./loading.css";
 
 
@@ -25,11 +30,22 @@ const Loading = () => {
     return ( 
     <AnimatedPage>
     <div className='loading-page-container d-flex flex-column justify-content-center align-items-center'>
-            
-        <div className=' d-flex loading-page-image-container'>
-            <img src={phub} width={400} alt="loading" className="loading-image" />
-        </div>
 
+        <motion.div
+                    initial={{ opacity: .5, scale: 1 }}
+                    animate={{ opacity: 1, scale: 3 }}
+                    transition={{
+                        duration: 2,
+                    }}
+                    ease="easeOut"
+                >
+                    <div className=' d-flex loading-page-image-container'>
+                        <img src={phub} width={400} alt="loading" className="loading-image" />
+                    </div>
+
+        </motion.div>
+                
+       
 
     </div> 
     </AnimatedPage>
